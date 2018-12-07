65bee10f6e52d640486605c6d677c9477e914703
Luís Coelho 1160754 <1160754@isep.ipp.pt>
2018-12-07 00:40:38 +0000

    teste

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/SiCCliente/src'));
+app.use(express.static(__dirname + '/dist/SICCLIENTE/src'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/SiCCliente/src/index.html'));
+res.sendFile(path.join(__dirname+'/SICCLIENTE/src/index.html'));
 });
 
 
